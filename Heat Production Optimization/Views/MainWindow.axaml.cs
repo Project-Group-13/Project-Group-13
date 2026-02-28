b169ae5753437ede15a71f789e169843c8ba4c02
daniel-morg-silva <dms121970@gmail.com>
2026-02-28 16:43:33 +0100

    feat: Initial Project Architecture

---
+++ Heat Production Optimization/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Heat_Production_Optimization.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
