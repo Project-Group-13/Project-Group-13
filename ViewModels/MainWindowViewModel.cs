f05c19fe84136b0600d28184615b7eb48b7b1a4f
Asen Tsarev <astsa25@student.sdu.dk>
2026-03-15 19:32:00 +0100

    UI overhaul + Graphs tab added

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,11 @@
-﻿namespace project.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace project.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    public GraphsViewModel GraphsVM { get; } = new();
+    public UnitsViewModel UnitsViewModel { get; } = new();
 }
--- obj/Debug/net10.0/project.AssemblyInfo.cs
+++ obj/Debug/net10.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a9a0e6600b36c4fb0925c4206c976b08d4e1392")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
