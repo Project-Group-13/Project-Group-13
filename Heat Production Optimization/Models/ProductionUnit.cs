ed13df41313f07a6ab8200493825b1d71bd9a361
Krasimir <gergankrasimir11@gmail.com>
2026-05-19 16:35:08 +0200

    merge fix

--- Heat Production Optimization/Models/ProductionUnit.cs
+++ Heat Production Optimization/Models/ProductionUnit.cs
@@ -12,14 +12,19 @@ public class ProductionUnit : INotifyPropertyChanged
 
     public int UnitId { get; set; }
     public string Name { get; set; } = "";
+
     public double MaxHeat { get; set; }      // MW
     public double ProductionCost { get; set; } // DkK/MWh
     public double? Co2Emissions { get; set; }  // kg/MWh
+
     public double? EnergyRate { get; set; }
     public double? MaxElectricity { get; set; }
+
     public string EnergyType { get; set; } = "";
     public string ImagePath { get; set; } = "";
     public int? GridId { get; set; }
+
+    public string UnitType { get; set; } = "";
 
     private Bitmap? _image;
 
